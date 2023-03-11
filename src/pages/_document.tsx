4de68c8b340f3cd4c84270840e4b0bb7c7f4f75d
qX5z <127591101+qX5z@users.noreply.github.com>
2023-03-11 14:24:28 +0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,12 +16,12 @@ class AppDocument extends Document {
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/site.webmanifest" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#FFFFFF" />
-          <meta property="og:url" content="https://erensezen.com.tr" />
+          <meta property="og:url" content="https://erensezen.com.tr/" />
           <meta property="og:site_name" content="Eren Sezen" />
           <meta property="og:image" content="/img/nebula.png" />
           <meta name="theme-color" content="#000000" />
           <meta name="twitter:card" content="summary" />
-          <meta name="twitter:image" content="https://itsnebula.net/img/nebula.png" />
+          <meta name="twitter:image" content="https://erensezen.com.tr/" />
         </Head>
         <body className="bg-white dark:bg-black text-black dark:text-white">
           <Main />
