53e83aa3ad49ac9dc99b3655fd6492e706aa06c2
qX5z <127591101+qX5z@users.noreply.github.com>
2023-03-11 12:06:55 +0300

    Update routes.ts

--- src/data/routes.ts
+++ src/data/routes.ts
@@ -26,4 +26,28 @@ export const RouteData: Route[] = [
     href: '/setup',
     type: 'dropdown',
   },
+]import { IconProp } from '@fortawesome/fontawesome-svg-core'
+
+export interface Route {
+  name: string
+  href: string
+  type?: string
+  icon?: IconProp
+}
+
+export const RouteData: Route[] = [
+  {
+    name: 'home',
+    href: '/',
+    icon: ['fas', 'home'],
+  },
+  {
+    name: 'projects',
+    href: '/projects',
+  },
+  {
+    name: 'eren',
+    href: '/blog',
+  },
 ]
+
